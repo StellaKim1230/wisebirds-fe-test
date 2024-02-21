6b735421c7d450dd85c0312882f3b2bb236098b4
StellaKim1230 <stellakim1230@gmail.com>
2024-02-21 15:18:33 +0900

    Feat: me정보를 layout에서 호출하도록 구현했다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,29 @@
-import Header from '../components/Header';
+import { use } from 'react';
 import { MSWComponent } from '../mocks/MSWComponent';
 import { ChakraProvider } from './providers';
+import Header from '../components/Header';
+import { Member } from '../types/member';
 
 export const metadata = {
   title: '와이즈버즈 프론트엔드 과제',
   description: '와이즈버즈 프론트엔드 과제입니다.',
 };
 
+async function getMe() {
+  const response = await fetch('http://localhost:4000/api/auth/me');
+  const me = await response.json();
+  return me;
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const me: Member = use(getMe());
+
   return (
     <html lang="en">
       <body>
         <ChakraProvider>
           <MSWComponent>
-            <Header />
+            <Header me={me} />
             <main>{children}</main>
           </MSWComponent>
         </ChakraProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-import { use } from 'react';
-import styles from './page.module.css';
-import { Member } from '../types/member';
-
-async function getMe() {
-  const response = await fetch('http://localhost:4000/api/auth/me');
-  const me = await response.json();
-  return me;
-}
-
 export default function Main() {
-  const me: Member = use(getMe());
-
-  return <main className={styles.main}>{me.name}</main>;
+  return <>main 페이지</>;
 }
