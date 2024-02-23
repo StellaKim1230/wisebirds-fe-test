e2abdff78eef0707096fc2e2c1b01f1d43c12dc8
StellaKim1230 <stellakim1230@gmail.com>
2024-02-23 12:15:31 +0900

    Fix: me 더미데이터 선언 위치를 옮겼다.

--- src/mocks/data/me.ts
+++
@@ -1,9 +0,0 @@
-export const me = {
-  id: 1,
-  email: 'abc@abc.com',
-  name: '홍길동',
-  company: {
-    id: 1,
-    name: '와이즈버즈',
-  },
-};
--- src/mocks/handlers/meHandler.ts
+++ src/mocks/handlers/meHandler.ts
@@ -1,6 +1,15 @@
 import { HttpResponse, http } from 'msw';
-import { me } from '../data/me';
 
 export const getMe = http.get('/api/auth/me', () => {
+  const me = {
+    id: 1,
+    email: 'abc@abc.com',
+    name: '홍길동',
+    company: {
+      id: 1,
+      name: '와이즈버즈',
+    },
+  };
+
   return HttpResponse.json(me);
 });
