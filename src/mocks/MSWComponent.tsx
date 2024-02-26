b89254d79ae6f1584a508099e481194fc9184562
StellaKim1230 <stellakim1230@gmail.com>
2024-02-26 10:10:00 +0900

    Fix: mock 서버를 개발모드일 경우에만 동작하도록 수정했다.

--- src/mocks/MSWComponent.tsx
+++ src/mocks/MSWComponent.tsx
@@ -4,17 +4,22 @@ import { useEffect, useState } from 'react';
 
 export const MSWComponent = ({ children }: { children: React.ReactNode }) => {
   const [mswReady, setMswReady] = useState(false);
-  useEffect(() => {
-    const init = async () => {
+  const isDev = process.env.NEXT_PUBLIC_API_MOCKING === 'enabled';
+
+  const init = async () => {
+    if (isDev) {
       const initMsw = await import('./index').then((res) => res.initMsw);
       await initMsw();
       setMswReady(true);
-    };
-
-    if (!mswReady) {
-      init();
     }
+  };
+
+  useEffect(() => {
+    if (mswReady) return;
+    init();
   }, [mswReady]);
+
+  if (!isDev) return null;
 
   return <>{children}</>;
 };
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,9 +1,13 @@
 export async function initMsw() {
   if (typeof window === 'undefined') {
     const { server } = await import('./server');
-    server.listen();
+    server.listen({
+      onUnhandledRequest: 'bypass',
+    });
   } else {
     const { worker } = await import('./browser');
-    await worker.start();
+    await worker.start({
+      onUnhandledRequest: 'bypass',
+    });
   }
 }
